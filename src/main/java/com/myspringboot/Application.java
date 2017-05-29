8c53e419a0439f9ac172a93bd8c2fa66ec8e6dcc
SnailFastGo <181223860@qq.com>
2017-05-30 02:09:38 +0800

    eclipse提交 添加注视

--- src/main/java/com/myspringboot/Application.java
+++ src/main/java/com/myspringboot/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @SpringBootApplication
 @ServletComponentScan
 public class Application {
+	//springboot启动程序入口
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
