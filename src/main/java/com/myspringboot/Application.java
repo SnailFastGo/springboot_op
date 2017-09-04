1e76e3eae827b17463073026a24b108f4d030395
dzn <18612424587@163.com>
2017-09-04 15:46:09 +0800

    将扫描包的注解由ServletComponentScan修改成ComponentScan

--- src/main/java/com/myspringboot/Application.java
+++ src/main/java/com/myspringboot/Application.java
@@ -2,10 +2,10 @@ package com.myspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ServletComponentScan
+@ComponentScan
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
